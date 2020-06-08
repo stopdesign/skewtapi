506405afbdd53ff3ea323adc87d1893db776fedc
John Kealy <johnckealy@gmail.com>
2020-06-08 14:14:38 +0100

    added static root

--- config/settings.py
+++ config/settings.py
@@ -138,7 +138,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-# STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 # WEBPACK_LOADER = {
 #     'DEFAULT': {
